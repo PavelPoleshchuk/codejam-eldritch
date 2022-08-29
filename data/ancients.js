cdb3b71bc590741d3c8e08258a69875bdf8543ca
PavelPoleshchuk <ppl@vivanta.by>
2022-08-29 15:54:17 +0300

    fix: changed card arrays

--- data/ancients.js
+++ data/ancients.js
@@ -3,9 +3,9 @@ export const ancientsData = [
     name: "azathoth",
     cardFace: "url(./assets/Ancients/Azathoth.png)",
     stages: [
-      [1, 1, 2],
-      [2, 1, 3],
-      [2, 0, 4],
+      [1, 2, 1],
+      [2, 3, 1],
+      [2, 4, 0],
     ]
   },
   {
@@ -13,26 +13,26 @@ export const ancientsData = [
     cardFace: "url(./assets/Ancients/Cthulthu.png)",
     stages: [
       [0, 2, 2],
-      [1, 0, 3],
-      [3, 0, 4],
+      [1, 3, 0],
+      [3, 4, 0],
     ]
   },
   {
     name: "iogSothoth",
     cardFace: "url(./assets/Ancients/IogSothoth.png)",
     stages: [
-      [0, 1, 2],
-      [2, 1, 3],
-      [3, 0, 4],
+      [0, 2, 1],
+      [2, 3, 1],
+      [3, 4, 0],
     ]
   },
   {
     name: "shubNiggurath",
     cardFace: "url(./assets/Ancients/ShubNiggurath.png)",
     stages: [
-      [1, 1, 2],
-      [3, 1, 2],
-      [2, 0, 4],
+      [1, 2, 1],
+      [3, 2, 1],
+      [2, 4, 0],
     ]
   }
 ];
