75226ae6d0442ac431f8b26b9b323b2a85529ee2
PavelPoleshchuk <ppl@vivanta.by>
2022-08-29 14:07:22 +0300

    fix: links in ancients.js

--- data/ancients.js
+++ data/ancients.js
@@ -1,7 +1,7 @@
 export const ancientsData = [
   {
     name: "azathoth",
-    cardFace: "url(/assets/Ancients/Azathoth.png)",
+    cardFace: "url(./assets/Ancients/Azathoth.png)",
     stages: [
       [1, 1, 2],
       [2, 1, 3],
@@ -10,7 +10,7 @@ export const ancientsData = [
   },
   {
     name: "cthulhu",
-    cardFace: "url(/assets/Ancients/Cthulthu.png)",
+    cardFace: "url(./assets/Ancients/Cthulthu.png)",
     stages: [
       [0, 2, 2],
       [1, 0, 3],
@@ -19,7 +19,7 @@ export const ancientsData = [
   },
   {
     name: "iogSothoth",
-    cardFace: "url(/assets/Ancients/IogSothoth.png)",
+    cardFace: "url(./assets/Ancients/IogSothoth.png)",
     stages: [
       [0, 1, 2],
       [2, 1, 3],
@@ -28,7 +28,7 @@ export const ancientsData = [
   },
   {
     name: "shubNiggurath",
-    cardFace: "url(/assets/Ancients/ShubNiggurath.png)",
+    cardFace: "url(./assets/Ancients/ShubNiggurath.png)",
     stages: [
       [1, 1, 2],
       [3, 1, 2],
